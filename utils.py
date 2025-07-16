e55c13ed5350f32981ab6e12df6b9ebed8373037
John Feng <johnfengphd@gmail.com>
2025-07-16 10:52:24 -0700

    Add dont_sleep function and refactor voltages_log_space parameters in utils.py
    - Introduced dont_sleep function to automate the "Don't Sleep!" command using pyautogui.
    - Refactored voltages_log_space function to improve parameter naming and handling of voltage ranges.
    - Enhanced functionality to support additional cases for voltage generation, including near-zero handling and rounding options.
    - Updated main execution block to demonstrate the new dont_sleep function.

--- utils.py
+++ utils.py
@@ -3,6 +3,13 @@ import pyautogui
 import time
 import numpy as np
 
+
+def dont_sleep():
+    pyautogui.press("win")
+    pyautogui.typewrite("Don't Sleep!", interval=0.05)
+    pyautogui.press("esc")
+
+
 def countdown_timer(seconds, action_interval=300):
     """Countdown timer for specified seconds"""
     logger.warning("Countdown Timer: " + str(seconds) + " seconds")
@@ -19,27 +26,33 @@ def countdown_timer(seconds, action_interval=300):
             pyautogui.typewrite("Don't Sleep!", interval=0.05)
             pyautogui.press("esc")
 
-def voltages_log_space(min_voltage, max_voltage, data_points):
+def voltages_log_space(start_voltage:int, 
+                       stop_voltage:int, 
+                       data_points:int, 
+                       near_zero=0.01,
+                       round_decimal=None):
     """
     Generate a list of voltages in a logarithmic space between min_voltage and max_voltage.
     The list will have data_points number of elements.
     """
-    near_zero = 0.01 # a number that is almost zero
-    if min_voltage > max_voltage or min_voltage==max_voltage:
-        raise ValueError(f"min_voltage ({min_voltage}) cannot be greater than or equal to max_voltage ({max_voltage})")
-    
-    if min_voltage < 0 and max_voltage > 0:
-        negatives = -1 * np.geomspace(abs(min_voltage), near_zero, round(data_points / 2))
-        positives = np.geomspace(near_zero, abs(max_voltage), round(data_points / 2))
+    # near_zero = 0.01 # a number that is almost zero
+    if start_voltage < 0 and stop_voltage > 0:
+        negatives = -1 * np.geomspace(abs(start_voltage), near_zero, round(data_points / 2))
+        positives = np.geomspace(near_zero, abs(stop_voltage), round(data_points / 2))
         voltages = np.concatenate([negatives, positives])
-    elif min_voltage < 0 and max_voltage <= 0:
-        if max_voltage == 0:
-            max_voltage = near_zero
-        voltages = -1 * np.geomspace(abs(min_voltage), abs(max_voltage), data_points)
-    elif min_voltage >= 0 and max_voltage >= 0:
-        if min_voltage == 0:
-            min_voltage = near_zero
-        voltages = np.geomspace(min_voltage, max_voltage, data_points)
+    elif start_voltage < 0 and stop_voltage <= 0:
+        if stop_voltage == 0:
+            stop_voltage = near_zero
+        voltages = -1 * np.geomspace(abs(start_voltage), abs(stop_voltage), data_points)
+    elif start_voltage >= 0 and stop_voltage >= 0:
+        if start_voltage == 0:
+            start_voltage = near_zero
+        voltages = np.geomspace(start_voltage, stop_voltage, data_points)
+    elif stop_voltage < 0 and start_voltage >=0:
+        voltages = -1 * np.geomspace(near_zero, abs(stop_voltage), data_points)
+
+    if round_decimal is not None:
+        return np.round(voltages, round_decimal)
     return voltages
 
 def voltages_dual_direction(min_voltage, max_voltage, data_points):
@@ -54,16 +67,21 @@ def voltages_dual_direction(min_voltage, max_voltage, data_points):
 
 
 if __name__ == "__main__":
-    voltages = voltages_log_space(min_voltage=-1000, max_voltage=1000, data_points=100)
-    print(voltages)
+    dont_sleep()
+    time.sleep(2)
+    dont_sleep()
+    time.sleep(2)
+    dont_sleep()
+    # voltages = voltages_log_space(min_voltage=-1000, max_voltage=1000, data_points=100)
+    # print(voltages)
+    # # import matplotlib.pyplot as plt
+    # # plt.plot(voltages, '.-')
+    # # plt.grid(True)
+    # # plt.show()
+    # voltages = voltages_dual_direction(min_voltage=-1000, max_voltage=1000, data_points=100)
+    # print(voltages)
     # import matplotlib.pyplot as plt
     # plt.plot(voltages, '.-')
     # plt.grid(True)
     # plt.show()
-    voltages = voltages_dual_direction(min_voltage=-1000, max_voltage=1000, data_points=100)
-    print(voltages)
-    import matplotlib.pyplot as plt
-    plt.plot(voltages, '.-')
-    plt.grid(True)
-    plt.show()
 
